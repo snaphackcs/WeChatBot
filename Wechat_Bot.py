da1f466a4f518a9905f67147f8fc853aad76a59b
KevenLin99 <93110982+KevenLin99@users.noreply.github.com>
2022-09-10 23:51:44 +0800

    Update Wechat_Bot.py

--- Wechat_Bot.py
+++ Wechat_Bot.py
@@ -14,41 +14,51 @@ wechat = ntchat.WeChat()
 wechat.open(smart=True)
 # 等待登录
 wechat.wait_login()
-# 发信息冷却
+# 色图冷却计时器，防止有老六访问量过大
 setu_time = 0
+# 贤者时间语句计时器
 xianzhe_time = 0
-last_path = ""
+# 读取群友信息
 with open("name_dict.json", mode="r", encoding="utf-8") as f:
     name_dict = load(f)
 print(name_dict)
+# 提醒群友bot已经启动
 wechat.send_text(to_wxid="23278031443@chatroom", content="bot已启动，目前支持发色图和签到哦 ᕕ( ᐛ )ᕗ")
 
 
+# 监测到有人发了消息
 @wechat.msg_register(ntchat.MT_RECV_TEXT_MSG)
+# 用于发色图的函数
 def send_setu(wechat_instance: ntchat.WeChat, message):
+    # 利用全局变量来读取两个计时器的值
     global setu_time
     global xianzhe_time
+    # 获取群中消息的信息
     data = message["data"]
+    # 获取信息的具体内容
     msg = data["msg"]
+    # 获取消息是在哪一个群
     room_wxid = data["room_wxid"]
-    if room_wxid == "23278031443@chatroom" and msg.lower() == "/setu":
+    # 判断是否在SnapHack群使用的色图指令
+    if room_wxid == "23278031443@chatroom" and msg.lower() == "/setu" and time() - setu_time >= 3:
+        # 如果已经过了贤者时间
         if time() - setu_time >= 180:
+            # 将计时器更新到现在的时间
             setu_time = time()
+            # 爬取色图同时获取色图信息
             data = RandomSetu()
-            request_times = 0
-            while len(data[0]) == 0 and request_times < 10:
-                data = RandomSetu()
-                request_times += 1
-            for img in data[0]:
+            # 利用循环将所有访问到的色图全部发出去
+            for img in data:
                 print(img)
                 wechat_instance.send_image(to_wxid="23278031443@chatroom", file_path=img)
-            if data[1]:
-                wechat_instance.send_text(to_wxid="23278031443@chatroom", content="有些图被bot ban了, 不怪我 ᕕ( ᐛ )ᕗ")
-                sleep(1)
+            sleep(1)
             # 递归删除色图, 释放内存ᕕ( ᐛ )ᕗ
             rmtree("temp")
+        # 如果没有过贤者时间但是过了发送贤者时间冷却
         elif time() - xianzhe_time >= 1.5:
+            # 将计时器更新到现在的时间
             xianzhe_time = time()
+            # 输出贤者时间
             wechat_instance.send_text(to_wxid="23278031443@chatroom",
                                       content=f"贤者时间还有{'%.2f' % (180 - time() + setu_time)}秒")
 
