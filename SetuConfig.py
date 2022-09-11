53d2687e082b6752742c2e00caee2b97a04a6dc5
KevenLin99 <93110982+KevenLin99@users.noreply.github.com>
2022-09-11 13:11:24 +0800

    Add files via upload

---
+++ SetuConfig.py
@@ -0,0 +1,18 @@
+from json import dump
+import argparse
+
+tag = []
+parser = argparse.ArgumentParser()
+parser.add_argument("-R", "--r18", type=int, choices=[0, 1], default=0, help="0不为R18，1为R18")
+parser.add_argument("-N", "--num", type=int, choices=range(1, 21), default=1, help="爬取图片数目，在1到20之间")
+parser.add_argument("-T", "--tag", default="", nargs='*')
+args = vars(parser.parse_args())
+for i in args["tag"]:
+    s = "".join(f"{j}|" for j in i.split("/"))
+    tag.append(s)
+
+default_json = {"r18": args["r18"], "num": args["num"], "tag": tag}
+
+print(default_json)
+with open("config.json", mode="w") as f:
+    dump(default_json, f, indent=4)
