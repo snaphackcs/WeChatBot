f4f39b4fd722034115a491879127fe5e74803204
KevenLin99 <93110982+KevenLin99@users.noreply.github.com>
2022-09-14 10:53:07 +0800

    Update Sign.py

--- Sign.py
+++ Sign.py
@@ -1,4 +1,4 @@
-from json import load,dump
+from json import load, dump
 from datetime import datetime
 from BuildArchives import new_archive
 
@@ -22,17 +22,18 @@ def sign(from_wxid):
     # 建档
     if from_wxid not in origin.keys():
         new_archive(from_wxid)
-
+        with open("info.json", mode="r", encoding="utf-8") as f:
+            origin = load(f)
+            
     # 判断是否已签到并输出
-    if not origin[from_wxid]["sign_or_not"]:
-        origin[from_wxid]["score"] += 1
-        origin[from_wxid]["sign_or_not"] = True
-        with open("info.json", mode="w") as f:
-            dump(origin, f, indent=4)
-        return f"@{origin[from_wxid]['title']}{origin[from_wxid]['name']} 签到成功ᕕ( ᐛ )ᕗ, 算力++, 为{origin[from_wxid]['score']}点"
-
-    else:
+    if origin[from_wxid]["sign_or_not"]:
         return f"@{origin[from_wxid]['title']}{origin[from_wxid]['name']} 你已经签过了(｀Д´), 算力为{origin[from_wxid]['score']}点"
+    origin[from_wxid]["score"] += 1
+    origin[from_wxid]["sign_or_not"] = True
+    with open("info.json", mode="w") as f:
+        dump(origin, f, indent=4)
+    return f"@{origin[from_wxid]['title']}{origin[from_wxid]['name']} 签到成功ᕕ( ᐛ )ᕗ, 算力++, 为{origin[from_wxid]['score']}点"
+
 
 if __name__ == '__main__':
     print(sign("wxid_f42f8bssyu9312"))
