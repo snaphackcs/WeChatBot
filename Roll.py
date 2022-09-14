63b20e0e3b8ff91aa228808d242b18db74c51258
Zeyu Zhang <zhangzeyu115@gmail.com>
2022-09-14 09:21:56 +0800

    新增投骰子功能

---
+++ Roll.py
@@ -0,0 +1,20 @@
+import random
+from json import load
+from BuildArchives import new_archive
+
+def roll(from_wxid, msg):
+    # 读取人名列表
+    with open("name_dict.json", mode="r", encoding="utf-8") as f:
+        name_dict = load(f)
+    # 读取账户列表
+    with open("info.json", mode="r", encoding="utf-8") as f:
+        origin = load(f)
+
+    # 建档
+    if from_wxid not in origin.keys():
+        new_archive(from_wxid)
+
+    dice = eval(msg[5::])
+    return f"@{origin[from_wxid]['title']}{origin[from_wxid]['name']} 投了一个d{dice}，结果是{random.randint(1, dice)}ᕕ( ᐛ )ᕗ"
+
+print(roll("wxid_pdb55y5c8l5n12","/roll6"))
