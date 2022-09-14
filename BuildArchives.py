8965ed326d221c244e312013162c45c3941ffd74
KevenLin99 <93110982+KevenLin99@users.noreply.github.com>
2022-09-14 10:51:52 +0800

    Update BuildArchives.py

--- BuildArchives.py
+++ BuildArchives.py
@@ -1,4 +1,4 @@
-from json import load
+from json import load, dump
 
 
 def new_archive(from_wxid):
@@ -14,11 +14,15 @@ def new_archive(from_wxid):
         if from_wxid not in name_dict.keys():
             new_info_json = {from_wxid: "*姓名未录入*"}  # TODO: 读取群昵称
             name_dict.update(new_info_json)
-
-        new_info_json = {from_wxid: {
-            "name": name_dict[from_wxid],
-            "score": 0,
-            "sign_or_not": False,
-            "title": ""
-        }}
-        origin.update(new_info_json)
+            with open("name_dict.json", mode="w") as f:
+                dump(name_dict, f, indent=4)
+        else:
+            new_info_json = {from_wxid: {
+                "name": name_dict[from_wxid],
+                "score": 0,
+                "sign_or_not": False,
+                "title": ""
+            }}
+            origin.update(new_info_json)
+            with open("info.json", mode="w") as f:
+                dump(origin, f, indent=4)
