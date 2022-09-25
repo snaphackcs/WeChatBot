a2fb18da40f1b41d73dd5d380b7c84c65246e7e7
yyyanyy <yynemo123@gmail.com>
2022-09-25 10:23:20 +0800

    功能性测试

--- Wechat_Bot.py
+++ Wechat_Bot.py
@@ -13,6 +13,7 @@ from commands.Setu import random_setu, time_convert
 from conf.Fish import fish
 from conf.meitu import erciyuan
 from conf.Joke import joke
+from conf.feed import touwei
 import random
 # 创建微信
 wechat = ntchat.WeChat()
@@ -35,8 +36,8 @@ with open("config/room.json", mode="r", encoding="utf-8") as f:
     room = load(f)
 with open("config/bao.json", mode="r", encoding="utf-8") as f:
     bao = load(f)
-with open("temp.json", mode="w") as f:
-    tp = load(f)
+with open("config/wuping.json", mode="r", encoding="utf-8") as f:
+    wuping = load(f)
 # 提醒群友bot已经启动
 # wechat.send_text(to_wxid=room_wxid, content="bot已启动，已更新钓鱼！ ᕕ( ᐛ )ᕗ")
 
@@ -50,7 +51,8 @@ def bot(wechat_instance: ntchat.WeChat, message):
     global moyutime
     global lastmoyu
     global room
-    global tp
+    global bao
+    global wuping
     data = message["data"]
     msg = data["msg"]
     room_wxid = data["room_wxid"]
@@ -94,14 +96,24 @@ def bot(wechat_instance: ntchat.WeChat, message):
                 wechat.send_gif(to_wxid=room_wxid, file=r"C:\WeChatBot\1.gif")
             else:
                 wechat.send_gif(to_wxid=room_wxid, file=r"C:\WeChatBot\hua.gif")
+
         if msg == "/b":
-            item=""
-            for itemt in range(bao[from_wxid]):
+            item = "@ you have"
+            for itemt in (bao[from_wxid]):
                 if bao[from_wxid][itemt]!=0:
-                    item+=(f"{itemt}:{bao[from_wxid][itemt]}个")
-                    item+=(r"\n")
+                    item += ("\n")
+                    item += (f"{wuping[itemt]}:{bao[from_wxid][itemt]}个")
             wechat_instance.send_room_at_msg(to_wxid=room_wxid,
                                              content=item, at_list=[from_wxid])
+
+        elif msg[:5] == "/投喂":
+            if msg=="/投喂":
+                wechat_instance.send_room_at_msg(to_wxid=room_wxid,
+                                                 content=f"@{name_dict[from_wxid]} 要给bot酱投喂什么呢",
+                                                 at_list=[from_wxid])
+            else:
+                touwei(msg,from_wxid,room_wxid)
+
 
 
 
@@ -133,6 +145,10 @@ def bot(wechat_instance: ntchat.WeChat, message):
                     moyutime = 0
                 wechat_instance.send_image(to_wxid=room_wxid, file_path=erciyuan()[0])
                 lastmoyu = time()
+
+
+
+
 
         # 跑团
         elif msg[:5] == "/roll":
